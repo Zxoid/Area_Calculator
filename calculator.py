eb7d98cffb14659a0b623636ac896a57aa94d1dc
Emilio Julian Torres <97409383+Zxoid@users.noreply.github.com>
2025-10-30 19:53:21 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
-c
+print("==================")
+print("Area Calculator 📐")
+print("==================")
+
+print("1) Triangle")
+print("2) Rectangle")
+print("3) Square")
+print("4) Circle")
+print("5) Quit")
+
+shape = int(input("Which shape: "))
+base = int(input("Base: "))
+height = int(input("Height: "))
+
+if shape ==1:
+  area = base**2
+elif shape ==2:
+ area = base*height
+elif shape ==3:
+  area = (base*height)/2
+elif shape ==4:
+  area = 3.14*height**2
+
+    
